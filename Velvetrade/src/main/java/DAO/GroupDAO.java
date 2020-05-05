a1e12ea6620b579c2ee6e0a0c30d926d9f5fd1b6
Davin Seju <davsej100@gmail.com>
2020-05-05 15:57:23 -0700

    Basic Code Structure

---
+++ Velvetrade/src/main/java/DAO/GroupDAO.java
@@ -0,0 +1,4 @@
+package DAO;
+
+public interface GroupDAO {
+}
---
+++ Velvetrade/src/test/java/com/Velvetrade/Velvetrade/VelvetradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Velvetrade.Velvetrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelvetradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
