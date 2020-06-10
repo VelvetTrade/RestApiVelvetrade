a7192062cf39bcc94a9d8eceed49ea9f865bdfa4
Davin Seju <davsej100@gmail.com>
2020-06-09 20:15:15 -0700

    Password field added

--- Velvetrade/src/main/java/com/Velvetrade/Velvetrade/Model/User.java
+++ Velvetrade/src/main/java/com/Velvetrade/Velvetrade/Model/User.java
@@ -102,6 +102,7 @@ public class User {
         this.itemId = itemId;
         this.notifications = notifications;
         this.groups = groups;
+        this.password=password;
     }
 
     public String getId() {
